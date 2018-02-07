04ddeeb655fb1735dfdb7c310773f825d332cfe1
房一 <87649669@qq.com>
2018-02-07 09:59:53 +0800

    兼容4.4

--- fymvp/src/main/java/com/fangyi/fymvp/base/BaseApplication.java
+++ fymvp/src/main/java/com/fangyi/fymvp/base/BaseApplication.java
@@ -1,6 +1,8 @@
 package com.fangyi.fymvp.base;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 /**
  * ================================================
@@ -14,5 +16,10 @@ public abstract class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
